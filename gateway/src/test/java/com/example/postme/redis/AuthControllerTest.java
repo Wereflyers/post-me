9db1e2f27acea504e864e65edead16d2e6f26842
Ann <AcrimonyDoll@yandex.ru>
2023-10-08 19:49:26 +0300

    code style

--- gateway/src/test/java/com/example/postme/redis/AuthControllerTest.java
+++ gateway/src/test/java/com/example/postme/redis/AuthControllerTest.java
@@ -1,37 +1,57 @@
-/*package com.example.postme.redis;
+package com.example.postme.redis;
 
-import com.example.postme.user.dto.UserDto;
-import lombok.SneakyThrows;
+import com.example.postme.user.AuthController;
+import com.example.postme.user.AuthService;
+import com.example.postme.user.dto.JwtRequest;
+import com.example.postme.user.dto.RegistrationUserDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@ExtendWith(MockitoExtension.class)
 public class AuthControllerTest {
-    @SneakyThrows
-    @Test
-    void add_whenEmptyName_thenReturnValidationException() {
-        user1.setUsername("");
-        when(userService.add(user1)).thenReturn(user1);
+    @InjectMocks
+    private AuthController authController;
+    @Mock
+    private AuthService authService;
+    private JwtRequest user1;
+    private RegistrationUserDto registrationUserDto;
 
-        String result = mockMvc.perform(post("/users")
-                        .contentType("application/json")
-                        .content(objectMapper.writeValueAsString(userToCreate)))
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-
-        UserDto actualUser = userController.add(user1);
-
-        assertEquals(user1.getUsername(), actualUser.getUsername());
-        assertEquals(user1.getEmail(), actualUser.getEmail());
-        verify(userService).add(user1);
-
-        when(userRepository.findById(userId)).thenReturn(Optional.empty());
-
-        assertThrows(NullPointerException.class, () -> userService.get(userId));
-        verify(userRepository, atMostOnce()).findById(userId);
+    @BeforeEach
+    public void create() {
+        user1 = new JwtRequest("username", "password");
+        registrationUserDto = new RegistrationUserDto("username", "password", "password", "email@mail.ru");
     }
-}*/
+
+    @Test
+    public void createAuthTokenTest() {
+        when(authService.createAuthToken(user1)).thenReturn(ResponseEntity.accepted().body(user1));
+
+        JwtRequest actualUser = (JwtRequest) authController.createAuthToken(user1).getBody();
+
+        assert actualUser != null;
+        assertEquals(actualUser.getUsername(), user1.getUsername());
+        assertEquals(actualUser.getPassword(), user1.getPassword());
+        verify(authService).createAuthToken(user1);
+    }
+
+    @Test
+    public void createNewUserTest() {
+        when(authService.createNewUser(registrationUserDto)).thenReturn(ResponseEntity.accepted().body(registrationUserDto));
+
+        RegistrationUserDto actualUser = (RegistrationUserDto) authController.createNewUser(registrationUserDto).getBody();
+
+        assert actualUser != null;
+        assertEquals(actualUser.getUsername(), registrationUserDto.getUsername());
+        assertEquals(actualUser.getPassword(), registrationUserDto.getPassword());
+        verify(authService).createNewUser(registrationUserDto);
+    }
+}
